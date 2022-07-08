4958d626df53bcf4b4f8d109b460f8113874bc08
hayoon45 <48703206+hayoon45@users.noreply.github.com>
2022-07-08 22:08:31 +0900

    generate

---
+++ src/main/java/com/project/cafemoa/CafemoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.cafemoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cafemoa/CafemoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cafemoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafemoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
