b4411e93d96109c61d15a09bb93bbe5ee90781fc
hayoon45 <48703206+hayoon45@users.noreply.github.com>
2022-07-20 21:38:49 +0900

    test

--- src/main/java/com/project/cafemoa/domain/user/User.java
+++ src/main/java/com/project/cafemoa/domain/user/User.java
@@ -13,7 +13,6 @@ import javax.persistence.*;
 @Builder
 @Getter
 @Entity
-@Table(name = "USERS")
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increase
